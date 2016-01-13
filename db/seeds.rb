e3a53425cfd390df5cb7baa94c19379608d00a01
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-13 14:53:42 -0500

    Site built with mock products, prices, and skus

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.destroy_all
+
+1000.times do
+  Product.create(name:Faker::Commerce.product_name, price:Faker::Commerce.price, sku:Faker::Code.isbn)
+end
